2dda94e165a2f4ea45e03fae66692c82769c4d2c
kandiahtk-crypto <kandiah.tk@gmail.com>
2026-03-19 11:50:06 +0000

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,11 @@
-
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/services">Services</a>
+        <a href="/contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
