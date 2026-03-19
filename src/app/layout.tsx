ce7476405bc21ad08fec9b2b6a8dca9eca7a4ced
kandiahtk-crypto <kandiah.tk@gmail.com>
2026-03-19 11:46:36 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "UK Inbound Ground Transport",
+  description: "Corporate website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
