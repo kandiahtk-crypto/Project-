d495c433793da8c7a738c84eb878897644916c01
kandiahtk-crypto <kandiah.tk@gmail.com>
2026-03-31 14:16:54 +0100

    Refactor robots function to use hardcoded sitemap URL
    Update sitemap URL to a hardcoded value.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,11 @@
 import type { MetadataRoute } from "next";
 
-const siteUrl = "https://www.ukinboundgroundtransport.com";
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${siteUrl}/sitemap.xml`,
-    host: siteUrl,
+    sitemap: "https://www.ukinboundgroundtransport.com/sitemap.xml",
   };
 }
